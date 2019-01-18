fed5357b3a970f20034b617b0860ce4a99acdbbe
Student <example@example.com>
2019-01-18 09:44:55 -0800

    map logic in scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+css
+q1 = look
+q2 = no
+q3 = no
+q4 = web layout
+
+c#
+q1 = function
+q2 = no
+q3 = yes
+q4 = web dev
+
+PHP
+q1 = function
+q2 = NULL
+q3 = no
+q4 = web dev
+
+
+Ruby
+q1 = NULL
+q2 = yes
+q3 = no
+q4 =  app dev
+
+Java
+q1 = NULL
+q2 = yes
+q3 = yes
+q4 =  app dev
+
+part / full
+links to each 
