f9502825752bc1bf5a4b2a5ee64f443afbce1ca8
Student <example@example.com>
2019-01-18 14:35:28 -0800

    complete logic along with code clean up, begin README.md

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,9 @@ $(document).ready(function(){
     var q3Input = $("#q3").val();
     var q4Input = $("#q4").val();
     var q5Input = $("#q5").val();
-    var q5InputFull = $("#q5full").val();
-    var q5InputPart = $("#q5part").val();
+    var q6Input = $("#q6").val();
+    // var q5InputFull = $("#q5full").val();
+    // var q5InputPart = $("#q5part").val();
 
     $(".name").text(nameInput);
     $(".q1").text(q1Input);
@@ -17,48 +18,38 @@ $(document).ready(function(){
     $(".q3").text(q3Input);
     $(".q4").text(q4Input);
     $(".q5").text(q5Input);
-
+    $(".q6").text(q6Input);
 
 
     if(nameInput ==="") {
     alert("Please enter your name.")
 
-  }else if (q1Input === "Aesthetics of a website" && q2Input === "No" && q3Input === "No" && q4Input === "Web layout"){
+  }else if (q1Input === "Aesthetics of a website" || q1Input === "Both, equally" && q2Input === "No" || q2Input === "Yes" && q3Input === "No" || q3Input === "Yes" && q4Input === "Web layout" && q5Input === "Yes"){
     $("#user-result").show();
     $("#cssResult").show();
     $("#cssResult").siblings().hide();
 
-    }else if(q1Input === "Overall functionality of a website" && q2Input === "No" && q3Input === "Yes" && q4Input === "Web development"){
+  }else if(q1Input === "Overall functionality of a website" && q2Input === "No" && q3Input === "Yes" && q4Input === "Web development" && q5Input === "No"){
     $("#user-result").show();
     $("#cResult").show();
     $("#cResult").siblings().hide();
 
-    }else if(q1Input === "Overall functionality of a website" && q2Input === "No" && q3Input === "No" && q4Input === "Web development"){
+  }else if(q1Input === "Overall functionality of a website" || q1Input === "Both, equally" && q2Input === "No" && q3Input === "No" || q3Input === "Yes" && q4Input === "Web development" && q5Input === "No"){
     $("#user-result").show();
     $("#phpResult").show();
     $("#phpResult").siblings().hide();
 
-    }else if(q1Input ==="Overall functionality of a website" && q2Input === "Yes" && q3Input === "No" && q4Input === "App development"){
+  }else if(q1Input === "Overall functionality of a website" && q2Input === "Yes" && q3Input === "No" && q4Input === "App development" && q5Input === "Yes" || q5Input === "No" || q5Input === "Yes"){
     $("#user-result").show();
     $("#rubyResult").show();
     $("rubyResult").siblings().hide();
 
-  }else if(q1Input ==="Both, equally" && q2Input ==="Yes" && q3Input === "Yes" && q4Input === " App development" || " Web development"){
+  }else if(q1Input === "Both, equally" || q1Input === "Overall functionality of a website" && q2Input === "Yes" && q3Input === "Yes" || q3Input === "No" && q4Input === "App development" || q4Input === "Web development" && q5Input === "No" || q5Input === "Yes"){
     $("#user-result").show();
     $("#javaResult").show();
     $("#javaResult").siblings().hide();
 
-    // }else if (q5Input === "Full time"){
-    // $("#q5full").show;
-    // alert("full")
-    //
-    // }else if(q5Input === "Part time"){
-    // $("#q5part").show;
-    // alert("part")
   }
-    // }else () {
-
   event.preventDefault();
-
   });
 });
